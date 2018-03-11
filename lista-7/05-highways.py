6ec53eccf0ff0d3aa2b3dec7e459a496aa2dd35b
Daniel Rodrigues Coura <danielrcifpb@gmail.com>
2018-03-10 21:43:12 -0300

    Update 05-highways.py

--- lista-7/05-highways.py
+++ lista-7/05-highways.py
@@ -1,5 +1,5 @@
 from heapq import heappush, heappop
- 
+
 def dijkstra(s, c, num_vert, graph):
 	visit = [False] * (num_vert + 1)
 	dist = [9999999999] * (num_vert + 1)
@@ -16,17 +16,17 @@ def dijkstra(s, c, num_vert, graph):
 		for e in graph[v[1]]:
 			if v[0] + e[1] < dist[e[0]]:
 				heappush(h, ((v[0] + e[1], e[0])))
- 
+
 	if dist[c] == 9999999999:
 		return 'NONE'
 	return dist[c]
- 
+
 def cria_grafo(v):
 	grafo = []
 	for i in xrange(v + 1):
 		grafo.append([])
 	return grafo
- 
+
 loop = int(raw_input())
 for i in xrange(loop):
 	v, k, saida, chegada = map(int, raw_input().split())
@@ -35,5 +35,5 @@ for i in xrange(loop):
 		a, b, c = map(int, raw_input().split())
 		grafo[a].append((b, c))
 		grafo[b].append((a, c))
- 
-	print dijkstra(saida, chegada, v, grafo) 
+
+	print dijkstra(saida, chegada, v, grafo)
